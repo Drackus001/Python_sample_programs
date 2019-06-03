29a6914e49575037e60e41c8999b04ee7fddec4d
root <root@localhost.localdomain>
2019-06-03 15:40:47 +0530

    modified 2_string.py and create new file called 3_numbers.py

---
+++ 2_string.py
@@ -0,0 +1,13 @@
+#message = 'I'm glad that you are here to watch my code ;)' #invalid syntax error
+
+message = 'I\'m glad that you are here to watch my code ;)' #use backslash(\) to use singleqoute(') in string
+print(message)
+
+#message2 = "You are "awesome" do you know that?" #invalid syntax
+message2 = 'You are "awesome" do you know that?'
+print(message2)
+
+message3 = """This is used for to store "multiline"
+    string into string (with all qoutes).
+    don't forget to give a thum's up to this code."""
+print(message3)
---
+++ 3_numbers.py
@@ -0,0 +1,15 @@
+#3 types of number
+#   1. int
+#   2. float
+#   3. complex
+
+i = 786
+print("type(i): ",type(i)) #show type of a
+
+f = 3.14
+print("type(f): ", type(f)) #show the type of f
+
+z = 2 - 6.1j
+print("type(z): ", type(z)) #show the type of z
+print("z.real: ", z.real) #complex store i in float
+print("z.imag: ", z.imag) #complex store j in float
